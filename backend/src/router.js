601641234c013b174e0b124a7e104001efef655a
Lucas <lucas_carrvetta@hotmail.com>
2024-05-26 18:21:15 -0300

    retirada de comentarios

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,5 +11,3 @@ router.delete('/users', usersController.removeUserByEmail);
 router.delete('/users/deleteall', usersController.removeAll);
 
 module.exports = router;
-//min 21:30
-//https://www.youtube.com/watch?v=Cdu0WJhI-d8&t=1194s
